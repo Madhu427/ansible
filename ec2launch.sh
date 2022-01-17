b8dfe79f52e59f5af6e68dddea6c0c74264aeaaf
Madhu427 <madhusudhanachary.k@gmail.com>
2022-01-17 20:19:59 +0530

    ec2 launch and spot json-- changed ec2.launch file

--- ec2launch.sh
+++ ec2launch.sh
@@ -3,4 +3,4 @@
 TEMP_ID="lt-0755cc2cbc460e5f3"
 TEMP_VER=1
 
-aws ec2 run-instances LaunchTemplateId=$(TEMP_ID),Version=$(TEMP_VER)
+aws ec2 run-instances LaunchTemplateId=${TEMP_ID},Version=${TEMP_VER}
