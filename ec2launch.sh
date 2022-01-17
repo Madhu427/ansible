f94c9fbe2f5a91aabe09673d9e00b8f259693d74
Madhu427 <madhusudhanachary.k@gmail.com>
2022-01-17 23:38:33 +0530

    creating spot and normal instance

--- ec2launch.sh
+++ ec2launch.sh
@@ -3,4 +3,4 @@
 TEMP_ID="lt-0755cc2cbc460e5f3"
 TEMP_VER=1
 
-aws ec2 run-instances --launch-template LaunchTemplateId=${TEMP_ID},Version=${TEMP_VER} --tag-specifications "ResourceType=instance,Tags=[{Key=name,Value=frontend}] ResourceType=spot-instances-request,Tags=[{Key=name,Value=frontend}]"
+aws ec2 run-instances --launch-template LaunchTemplateId=${TEMP_ID},Version=${TEMP_VER} --tag-specifications "ResourceType=instance,Tags=[{Key=name,Value=frontend}]" "ResourceType=spot-instances-request,Tags=[{Key=name,Value=frontend}]"
