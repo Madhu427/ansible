24db63341f1163d04ae55d116b09badb9bd21105
Madhu427 <madhusudhanachary.k@gmail.com>
2022-01-18 10:07:58 +0530

    cahnged version in ec2launch

--- ec2launch.sh
+++ ec2launch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 TEMP_ID="lt-0755cc2cbc460e5f3"
-TEMP_VER=1
+TEMP_VER=2
 
 aws ec2 run-instances --launch-template LaunchTemplateId=${TEMP_ID},Version=${TEMP_VER} --tag-specifications "ResourceType=instance,Tags=[{Key=name,Value=frontend}]" | jq
