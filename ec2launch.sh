3964f8b65ccdec55ad6d888e64c83e3c2a01d011
Madhu427 <madhusudhanachary.k@gmail.com>
2022-01-18 12:05:35 +0530

    shell script implemented on ec2launch

--- ec2launch.sh
+++ ec2launch.sh
@@ -3,4 +3,20 @@
 TEMP_ID="lt-0755cc2cbc460e5f3"
 TEMP_VER=5
 
-aws ec2 run-instances --launch-template LaunchTemplateId=${TEMP_ID},Version=${TEMP_VER} --tag-specifications "ResourceType=instance,Tags=[{Key=name,Value=frontend}]"  | jq
+if [ -z "$1" ] then
+  echo "Input missing"
+  exit
+fi
+
+COMPONENT=$1
+
+
+
+aws ec2 describe-instances --filters "Name=tag:name,Values=${COMPONENT}" | jq .Reservations[].Instances[].State.Name | sed 's/"//g' | grep -E "running|stopped" &>/dev/null
+
+if [ $? -eq 0 ]; then
+  echo "Instance is already there"
+  exit
+fi
+
+aws ec2 run-instances --launch-template LaunchTemplateId=${TEMP_ID},Version=${TEMP_VER} --tag-specifications "ResourceType=instance,Tags=[{Key=name,Value=${COMPONENT}]"  | jq
