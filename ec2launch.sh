7967bd7ce5af84bdc01099896ac85e2c1797f157
Madhu427 <madhusudhanachary.k@gmail.com>
2022-01-18 12:40:08 +0530

    shell script implemented on ec2launch

--- ec2launch.sh
+++ ec2launch.sh
@@ -4,7 +4,7 @@ TEMP_ID="lt-0755cc2cbc460e5f3"
 TEMP_VER=5
 
 if [ -z "$1" ]; then
-  echo "Input is missing"
+  echo -e "\e[1;31Input is missing\e[0m"
   exit
 fi
 
@@ -15,7 +15,7 @@ COMPONENT=$1
 aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}" | jq .Reservations[].Instances[].State.Name | sed 's/"//g' | grep -E "running|stopped" &>/dev/null
 
 if [ $? -eq 0 ]; then
-  echo "Instance is already there"
+  echo -e "\e[1;33mInstance is already there\e[0m"
   exit
 fi
 
