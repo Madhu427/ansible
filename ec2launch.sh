132237f2d3a1d7ecf95ab8b40de5b165c503be38
Madhu427 <madhusudhanachary.k@gmail.com>
2022-01-19 13:19:19 +0530

    [ "${COMPONENT}"=="all"]; added to ec2launch

--- ec2launch.sh
+++ ec2launch.sh
@@ -4,13 +4,15 @@ TEMP_ID="lt-06c5374ac5f24eb25"
 TEMP_VER=2
 ZONE_ID=Z0686471167SJWZUE9FVZ
 
+export COMPONENT=$1
+
 if [ -z "$1" ]; then
   echo -e "\e[1;31mInput is missing\e[0m"
   exit
 fi
 
-export COMPONENT=$1
 
+CREATE_INST() {
 
 
 aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}" | jq .Reservations[].Instances[].State.Name | sed 's/"//g' | grep -E "running|stopped" &>/dev/null
@@ -23,8 +25,19 @@ else
 
 fi
 
+sleep 10
 
 IPADDRESS=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}" | jq .Reservations[].Instances[].PrivateIpAddress | sed 's/"//g' | grep -v null)
 
 sed -e "s/IPADDRESS/${IPADDRESS}/" -e "s/COMPONENT/${COMPONENT}/" record.json >/tmp/record.json
 aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/record.json | jq
+}
+
+if [ "${COMPONENT}"=="all"]; then
+  for comp in frontend mongodb redis catalogue; do
+    COMPONENT=$comp
+    CREATE_INST
+  done
+  else
+    CREATE_INST
+fi
